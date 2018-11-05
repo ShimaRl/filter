41021c4a04fc4964984676d6cdcfb40383c9fd77
ShimaRl <shimavagheii@gmail.com>
2018-11-05 11:10:39 +0330

    checkbox filter fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, zip } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { AngularFireDatabase } from '@angular/fire/database';
 import {FirebaseListObservable, FirebaseObjectObservable} from '@angular/fire/database-deprecated';
 import { Apparel } from './apparel/apparel.model';
-import { filter, reduce, map } from 'rxjs/operators';
-import { element } from 'protractor';
+import { filter, reduce, map, zip, concat, merge } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -19,56 +18,81 @@ export class AppComponent implements OnInit {
   apparels: any;
   filteredApparels: any;
 
-  designer: string;
-  small: boolean;
-  medium: boolean;
-  large: boolean;
-
   filters: {key: string, val: any}[] =[];
+
+  checkboxObservable: any;
+  checkCount: number;
 
   ngOnInit(){
     this.db.list('/apparel').valueChanges().subscribe(apparels => {
       this.apparels = apparels;
       this.applyFilters();
-    })
-  }
-
-  applyFilters(){
-
-    this.filteredApparels = this.apparels;
-    if(this.filters.length != 0){
-      this.filters.forEach(element => {
-
-        if(typeof(element.val) == 'string'){
-            this.filteredApparels = this.filteredApparels.filter(x => x[element.key] == element.val);
-        }else if(typeof(element.val) == 'boolean'){
-          this.filteredApparels = this.filteredApparels.filter(x => x['shape'] == element.key);
-        }
-      });
-    }
-  }
-
-  filterString(key: string, rule: any) {
-    let index = this.filters.findIndex(i => i.key == key);
-    if(index != -1){
-      this.removeFilter(key);
-    }
-    this.filters.push({
-      key: key,
-      val: rule
     });
+    this.checkCount = 0;
+  }
+
+  onCheckboxChanged(event){
+
+    var key = event.target.parentElement.parentElement.parentElement.id;
+    var value = event.target.value;
+
+    if(!event.target.checked){// age check bardashte shod
+      this.checkCount--;
+      if(this.checkCount != 0){//age hanooz checkboxi vojood dare ke checked hast
+        this.checkboxObservable = this.checkboxObservable.filter(x => x[key] != value);
+      }else{//age hameye checkboxa unchecked hastan
+        this.checkboxObservable = null;
+      }
+    }else{//age check gozashte shod
+      this.checkCount++;
+      var o = this.apparels.filter(x => x[key] == value);//filtere kole dadeha bar asase checkboxe check shode     
+      if(this.checkboxObservable != null){//age az ghabl ham check boxe check shodei bood
+        var w =this.checkboxObservable;
+        this.checkboxObservable = this.checkboxObservable.concat(o);//ezafe kardan be ghablia
+      }else{
+        this.checkboxObservable = o;
+      }
+    }
     this.applyFilters();
   }
 
-  filterBoolean(key: string, rule: boolean) {
-    if (!rule){
-      this.removeFilter(key);
-    }
-    else {
-      this.filters.push({
+  onDropdownChanged(event) {
+    var key = event.target.id;
+    var value = event.target.value;
+    
+    if(value != ""){//age ye filter entekhab shod 
+      let index = this.filters.findIndex(i => i.key == key);
+      if(index != -1){//age filtere ba in key az ghabl vojood dasht
+        this.removeFilter(key);
+      }
+      this.filters.push({//ezafe kardane filtere jadid be araye filterha
         key: key,
-        val: rule
+        val: value
       });
+    }else{//age bardashtane filter entekhab shod 
+      this.removeFilter(key);      
+    }
+    
+    this.applyFilters();
+  }
+
+  onPriceSet(min, max){
+
+    var key = 'price';
+
+    if(min.value != "" && max.value != ""){//age ye filter entekhab shod 
+      if(min.value < max.value){
+        let index = this.filters.findIndex(i => i.key == key);
+        if(index != -1){//age filtere ba in key az ghabl vojood dasht
+          this.removeFilter(key);
+          }
+        this.filters.push({//ezafe kardane filtere jadid be araye filterha
+          key: key,
+          val: min.value + "*" + max.value
+        });
+      } 
+    }else{//age bardashtane filter entekhab shod 
+      this.removeFilter(key);      
     }
     this.applyFilters();
   }
@@ -76,6 +100,25 @@ export class AppComponent implements OnInit {
   removeFilter(key: string) {
     let index = this.filters.findIndex(i => i.key == key);
     this.filters.splice(index, 1);
+  }
+
+  applyFilters(){
+    if(this.checkboxObservable != null){//age hade aghal yeki az checkboxa checked bood
+      this.filteredApparels = this.checkboxObservable;//emale filtere checkboxa
+    }else{
+      this.filteredApparels = this.apparels;
+    }
+    if(this.filters.length != 0){//emale baghie filterha (hame be gheir az checkboxa)
+      this.filters.forEach(element => {
+        if(element.val.includes("*")){
+          var min = element.val.split("*")[0];
+          var max = element.val.split("*")[1];          
+          this.filteredApparels = this.filteredApparels.filter(x => x[element.key] >= min && x[element.key] <= max);
+        }else{
+          this.filteredApparels = this.filteredApparels.filter(x => x[element.key] == element.val);          
+        }
+      });
+    }
   }
 
 }
